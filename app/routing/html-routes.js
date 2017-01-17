bdd40372b1d0084be3b4eec76b2ff357c25da6a2
Your Name <shadburn.sean@gmail.com>
2017-01-17 15:24:35 -0500

    Second commit

---
+++ app/routing/html-routes.js
@@ -0,0 +1,8 @@
+app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"));
+});
+
+app.get("/", function (req, res) {
+  // res.send("Welcome to the Star Wars Page!")
+  res.sendFile(path.join(__dirname, "home.html"));
+});
